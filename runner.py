28ac633534bf3b5e919bc69282d6f38718ecf79b
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-03-02 19:33:03 -0400

    Update colors

--- runner.py
+++ runner.py
@@ -7,15 +7,17 @@ pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("font/pixeltype.ttf", 50)
 
-sky_surface = pygame.image.load("graphics/sky.png").convert()
-ground_surface = pygame.image.load("graphics/ground.png").convert()
-text_surface = test_font.render("Your game", False, "black")
+sky_surf = pygame.image.load("graphics/sky.png").convert()
+ground_surf = pygame.image.load("graphics/ground.png").convert()
 
-snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
-snail_rect = snail_surface.get_rect(midbottom = (600, 300))
+score_surf = test_font.render("Score", False, (64, 64, 64))
+score_rect = score_surf.get_rect(center = (400, 50))
 
-player_surface = pygame.image.load("graphics/player/player_walk_1.png").convert_alpha()
-player_rect = player_surface.get_rect(bottomright = (80, 300))
+snail_surf = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
+snail_rect = snail_surf.get_rect(midbottom = (600, 300))
+
+player_surf = pygame.image.load("graphics/player/player_walk_1.png").convert_alpha()
+player_rect = player_surf.get_rect(bottomright = (80, 300))
 
 while True:
     for event in pygame.event.get():
@@ -24,13 +26,15 @@ while True:
             exit()
         # if event.type == pygame.MOUSEMOTION:
         #    if player_rect.collidepoint(event.pos): print("collision")
-    screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (300, 50))
+    screen.blit(sky_surf, (0, 0))
+    screen.blit(ground_surf, (0, 300))
+    pygame.draw.rect(screen, "#c0e8ec", score_rect)
+    pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
+    screen.blit(score_surf, score_rect)
     snail_rect.x -= 4
     if snail_rect.right <= 0: snail_rect.left = 820
-    screen.blit(snail_surface, snail_rect)
-    screen.blit(player_surface, player_rect)
+    screen.blit(snail_surf, snail_rect)
+    screen.blit(player_surf, player_rect)
 
     # if player_rect.colliderect(snail_rect): 
     #     print("collision")
