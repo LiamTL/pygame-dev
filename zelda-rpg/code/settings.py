6533ca8ec2bf85ea94c3f10ab225ea59d8aa4e67
LiamTL <93681753+LiamTL@users.noreply.github.com>
2022-10-19 16:54:30 -0400

    Attempt To add FinalBoss

--- zelda-rpg/code/settings.py
+++ zelda-rpg/code/settings.py
@@ -51,5 +51,6 @@ magic_data = {
 monster_data = {
 	'squid': {'health': 100,'exp':100,'damage':20,'attack_type': 'slash', 'attack_sound':'C:/Users/tayel/Documents/Coding/pygame-dev/zelda-rpg/audio/attack/slash.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 80, 'notice_radius': 360},
 	'raccoon': {'health': 300,'exp':250,'damage':40,'attack_type': 'claw',  'attack_sound':'C:/Users/tayel/Documents/Coding/pygame-dev/zelda-rpg/audio/attack/claw.wav','speed': 2, 'resistance': 3, 'attack_radius': 120, 'notice_radius': 400},
+	'finalboss': {'health': 1000,'exp':2000,'damage':100,'attack_type': 'claw',  'attack_sound':'C:/Users/tayel/Documents/Coding/pygame-dev/zelda-rpg/audio/attack/claw.wav','speed': 2, 'resistance': 3, 'attack_radius': 50, 'notice_radius': 700},
 	'spirit': {'health': 100,'exp':110,'damage':8,'attack_type': 'thunder', 'attack_sound':'C:/Users/tayel/Documents/Coding/pygame-dev/zelda-rpg/audio/attack/fireball.wav', 'speed': 4, 'resistance': 3, 'attack_radius': 60, 'notice_radius': 350},
 	'bamboo': {'health': 70,'exp':120,'damage':6,'attack_type': 'leaf_attack', 'attack_sound':'C:/Users/tayel/Documents/Coding/pygame-dev/zelda-rpg/audio/attack/slash.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 50, 'notice_radius': 300}}
