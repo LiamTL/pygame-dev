2e8354b5f7d4fbb52ddcb95ff37e7b9f9a0ecbbd
LiamTL <93681753+LiamTL@users.noreply.github.com>
2023-03-20 15:22:04 -0400

    Complete Creating The Plane in python

--- flappybird/flappybird.py
+++ flappybird/flappybird.py
@@ -1,14 +1,14 @@
 import pygame, sys, time
 from settings import *
-from sprites import BG, Ground
+from sprites import BG, Ground, Plane
+
 
 class Game:
     def __init__(self):
-        
         # setup
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
-        pygame.display.set_caption('Flappy Bird')
+        pygame.display.set_caption('Flappy Plane')
         self.clock = pygame.time.Clock()
 
         # sprite groups
@@ -20,29 +20,33 @@ class Game:
         self.scale_factor = WINDOW_HEIGHT / bg_height
         # sprite setup
         BG(self.all_sprites, self.scale_factor)
+        Ground(self.all_sprites, self.scale_factor)
+        self.plane = Plane(self.all_sprites, self.scale_factor / 1.8)
 
-       
+
     def run(self):
         last_time = time.time()
         while True:
-            
+
             # delta time
             dt = time.time() - last_time
             last_time = time.time()
- 
+
             # event loop
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-            
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.plane.jump()
+
             # game logic
             self.all_sprites.update(dt)
             self.all_sprites.draw(self.display_surface)
 
             pygame.display.update()
             self.clock.tick(FRAMERATE)
- 
+
 if __name__ == '__main__':
     game = Game()
     game.run()
--- flappybird/sprites.py
+++ flappybird/sprites.py
@@ -23,7 +23,68 @@ class BG(pygame.sprite.Sprite):
         if self.rect.centerx <= 0:
             self.pos.x = 0
         self.rect.x = round(self.pos.x)
-
 class Ground(pygame.sprite.Sprite):
+
     def __init__(self, groups, scale_factor):
         super().__init__(groups)
+
+        # Image
+        ground_surf = pygame.image.load("../pygame-dev/flappybird/graphics/environment/ground.png").convert_alpha()
+        self.image = pygame.transform.scale(ground_surf, pygame.math.Vector2(ground_surf.get_size()) * scale_factor)
+
+        # Position
+        self.rect = self.image.get_rect(bottomleft = (0,WINDOW_HEIGHT))
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+    def update(self, dt):
+        self.pos.x -= 360 * dt
+        if self.rect.centerx <= 0:
+            self.pos.x = 0
+
+        self.rect.x = round(self.pos.x)
+
+class Plane(pygame.sprite.Sprite):
+    def __init__(self, groups, scale_factor):
+        super().__init__(groups)
+
+        # Image
+        self.import_frames(scale_factor)
+        self.frame_index = 0
+        self.image = self.frames[self.frame_index]
+
+        # Rect.
+        self.rect = self.image.get_rect(midleft = (WINDOW_WIDTH / 20, WINDOW_HEIGHT / 2))
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+
+        # Movement
+        self.gravity = 600
+        self.direction = 0
+
+    def import_frames(self, scale_factor):
+        self.frames = []
+        for i in range(3):
+            surf = pygame.image.load(f'C:/Users/tayel/Documents/Coding/pygame-dev/flappybird/graphics/plane/red{i}.png').convert_alpha()
+            scaled_surf = pygame.transform.scale(surf, pygame.math.Vector2(surf.get_size()) * scale_factor)
+            self.frames.append(scaled_surf)
+
+    def apply_gravity(self, dt):
+        self.direction += self.gravity * dt
+        self.pos.y += self.direction * dt
+        self.rect.y = round(self.pos.y)
+
+    def jump(self):
+        self.direction = -400
+
+    def animate(self, dt):
+        self.frame_index += 10 * dt
+        if self.frame_index >= len(self.frames):
+            self.frame_index = 0
+        self.image = self.frames[int(self.frame_index)]
+    
+    def rotate(self):
+        rotated_plane = pygame.transform.rotozoom(self.image, -self.direction * 0.06, 1)
+        self.image = rotated_plane
+
+    def update(self, dt):
+        self.apply_gravity(dt)
+        self.animate(dt)
+        self.rotate()
