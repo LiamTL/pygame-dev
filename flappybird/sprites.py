6475bceb8c2f52fa6b6fc0a36b8d821234cbcf12
LiamTL <93681753+LiamTL@users.noreply.github.com>
2023-03-31 17:03:26 -0400

    Add collisions for collisionables
    This adds collisions for the ground, plane, and obstacles.

--- flappybird/sprites.py
+++ flappybird/sprites.py
@@ -1,6 +1,6 @@
 import pygame
 from settings import *
-
+from random import choice, randint
 class BG(pygame.sprite.Sprite):
     def __init__(self, groups, scale_factor):
         super().__init__(groups)
@@ -35,6 +35,10 @@ class Ground(pygame.sprite.Sprite):
         # Position
         self.rect = self.image.get_rect(bottomleft = (0,WINDOW_HEIGHT))
         self.pos = pygame.math.Vector2(self.rect.topleft)
+
+        # mask
+        self.mask = pygame.mask.from_surface(self.image)
+
     def update(self, dt):
         self.pos.x -= 360 * dt
         if self.rect.centerx <= 0:
@@ -59,6 +63,9 @@ class Plane(pygame.sprite.Sprite):
         self.gravity = 600
         self.direction = 0
 
+        # mask
+        self.mask = pygame.mask.from_surface(self.image)
+
     def import_frames(self, scale_factor):
         self.frames = []
         for i in range(3):
@@ -79,12 +86,44 @@ class Plane(pygame.sprite.Sprite):
         if self.frame_index >= len(self.frames):
             self.frame_index = 0
         self.image = self.frames[int(self.frame_index)]
-    
+
     def rotate(self):
         rotated_plane = pygame.transform.rotozoom(self.image, -self.direction * 0.06, 1)
         self.image = rotated_plane
+        self.mask = pygame.mask.from_surface(self.image)
 
     def update(self, dt):
         self.apply_gravity(dt)
         self.animate(dt)
         self.rotate()
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, groups, scale_factor):
+        super().__init__(groups)
+
+        orientation = choice(("up", "down"))
+        surf = pygame.image.load(f'C:/Users/tayel/Documents/Coding/pygame-dev/flappybird/graphics/obstacles/{choice((0,1))}.png').convert_alpha()
+        self.image = pygame.transform.scale(surf, pygame.math.Vector2(surf.get_size()) * scale_factor)
+
+        x = WINDOW_WIDTH + randint(40, 100)
+
+        if orientation == "up":
+            y = WINDOW_HEIGHT + randint(10, 50)
+            self.rect = self.image.get_rect(midbottom = (x, y))
+        else:
+            y = randint(-50, -10)
+            self.image = pygame.transform.flip(self.image, False, True)
+            self.rect = self.image.get_rect(midtop = (x, y))
+
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+
+        # mask
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def update(self, dt):
+        self.pos.x -= 400 * dt
+        self.rect.x = round(self.pos.x)
+        if self.rect.right <= -100:
+            self.kill()
+
+
