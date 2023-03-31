d3a9c13e673baee575a7bdc86037ab03d63899a5
LiamTL <93681753+LiamTL@users.noreply.github.com>
2023-03-31 17:03:09 -0400

    Complete Creating The Obstacles in python

--- flappybird/flappybird.py
+++ flappybird/flappybird.py
@@ -1,6 +1,8 @@
+# P.S Time to return 2 is 1:08:25 Flappy Bird (IG u know dat...)
+
 import pygame, sys, time
 from settings import *
-from sprites import BG, Ground, Plane
+from sprites import BG, Ground, Plane , Obstacle
 
 
 class Game:
@@ -20,9 +22,17 @@ class Game:
         self.scale_factor = WINDOW_HEIGHT / bg_height
         # sprite setup
         BG(self.all_sprites, self.scale_factor)
-        Ground(self.all_sprites, self.scale_factor)
+        Ground([self.all_sprites, self.collision_sprites], self.scale_factor)
         self.plane = Plane(self.all_sprites, self.scale_factor / 1.8)
 
+        # timer
+        self.obstacle_timer = pygame.USEREVENT + 1
+        pygame.time.set_timer(self.obstacle_timer, 1400)
+
+    def collisions(self):
+        if pygame.sprite.spritecollide(self.plane, self.collision_sprites, False, pygame.sprite.collide_mask) or self.plane.rect.top <= 0:
+            pygame.quit()
+            sys.exit()
 
     def run(self):
         last_time = time.time()
@@ -40,7 +50,12 @@ class Game:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     self.plane.jump()
 
+                if event.type == self.obstacle_timer:
+                    Obstacle([self.all_sprites, self.collision_sprites], self.scale_factor)
+
             # game logic
+            self.display_surface.fill('black')
+            self.collisions()
             self.all_sprites.update(dt)
             self.all_sprites.draw(self.display_surface)
 
